652737dea41f56171f4e6b0e0f5318f3b6d8141c
Zephyrtoria <2594379136@qq.com>
2025-05-19 11:03:33 +0800

    admin: 新增员工

--- sky-common/src/main/java/com/sky/properties/JwtProperties.java
+++ sky-common/src/main/java/com/sky/properties/JwtProperties.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class JwtProperties {
 
     /**
-     * 管理端员工生成jwt令牌相关配置
+     * 管理端员工生成jwt令牌相关配置，通过配置文件注入
      */
     private String adminSecretKey;
     private long adminTtl;
